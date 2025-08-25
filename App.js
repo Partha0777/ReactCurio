b882e241699e30b0bc9987109ffa2301f31adadc
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-08-25 22:15:07 +0530

    Fixed height issue of list.

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SafeAreaView, View, Text, Button, StyleSheet, FlatList} from 'react-native';
 
-const Stack = createNativeStackNavigator();
 
+const Stack = createNativeStackNavigator();
 
 const sampleList = [
     {
