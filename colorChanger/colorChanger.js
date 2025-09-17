690b821cd58f15d396882e7aed9474c08906994d
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-09-17 23:06:23 +0530

    Added ecommerce splash scree.

--- colorChanger/colorChanger.js
+++ colorChanger/colorChanger.js
@@ -18,6 +18,7 @@ const reducer = (state, action) => {
 
 const ColorChanger = () => {
 
+
     const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0});
 
     const {red, green, blue} = state;
