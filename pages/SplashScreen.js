8e730645ef2c5a0aed71829e8a6dd62205667f35
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-09-20 22:14:21 +0530

    Added ecommerce splash scree.

--- pages/SplashScreen.js
+++ pages/SplashScreen.js
@@ -7,8 +7,11 @@ function SplashScreen({navigation}) {
     const {width, height} = useWindowDimensions()
 
     useEffect(() => {
+
         const t = setTimeout(() => navigation.replace('EcomHomeScreen'), 2000);
+
         return () => clearTimeout(t);
+
     }, [navigation]);
 
     return (<View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff'}}>
